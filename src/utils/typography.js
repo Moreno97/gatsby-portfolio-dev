bedd208f5ad62e82fb5b92f498aa5d2560878139
amoreno <amoreno@apsl.net>
2019-11-04 10:14:14 +0100

    Fix base font size

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,7 +1,7 @@
 import Typography from "typography";
 
 export default new Typography({
-  baseFontSize: "40px",
+  baseFontSize: "20px",
   baseLineHeight: 1.666,
   headerFontFamily: [
     "Avenir Next",
