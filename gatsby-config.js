a4e7e0ba93fa42396c443560c74cce8c7ac264aa
Antonio <amorenovalls@gmail.com>
2021-02-23 11:53:43 +0100

    UI changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,7 +25,7 @@ module.exports = {
       resolve: `gatsby-plugin-typography`,
       options: {
         pathToConfigModule: `src/utils/typography`,
-        omitGoogleFont: true,
+        // omitGoogleFont: true,
       }
     },
     "gatsby-transformer-sharp",
--- src/components/theme/Header/Hamburger/styles.js
+++ src/components/theme/Header/Hamburger/styles.js
@@ -16,8 +16,8 @@ export const Wrapper = styled.div`
   ${({ sidebar }) =>
     sidebar &&
     `
-			right: 18%;
-			top: 1.4rem;
+			right: 15%;
+			top: 1.6rem;
 		
 			@media (max-width: 960px) {
 				right: 35%;
@@ -30,37 +30,14 @@ export const Wrapper = styled.div`
 `;
 
 export const Bar = styled.div`
-	width: 1.4rem;
-	height: .15rem;
-	margin-bottom: .3rem;
-	background-color: #212121;
-	transition: transform 500ms cubic-bezier(0.6, 0.05, 0.28, 0.91),
-	opacity 500ms,
-	box-shadow 250ms,
-	background-color 500ms;
+  width: 1.4rem;
+  height: 0.15rem;
+  margin-bottom: 0.3rem;
+  background-color: #212121;
+  transition: transform 500ms cubic-bezier(0.6, 0.05, 0.28, 0.91), opacity 500ms,
+    box-shadow 250ms, background-color 500ms;
 
-	@media (max-width: 600px){
-		width: 1.6rem;
-	}
-
-	${({ top, sidebar }) =>
-    top &&
-    sidebar &&
-    `
-		transform: translateY(8px) rotate(-135deg);
-	`}
-
-	${({ mid, sidebar }) =>
-    mid &&
-    sidebar &&
-    `
-			transform: scale(0);
-	`}
-
-	${({ bottom, sidebar }) =>
-    bottom &&
-    sidebar &&
-    `
-			transform: translateY(-6px) rotate(-45deg);
-	`}
+  @media (max-width: 600px) {
+    width: 1.6rem;
+  }
 `;
